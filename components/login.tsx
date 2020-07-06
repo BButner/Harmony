cf9831e1d388762386a37d49c969470f5247bf61
BButner <beau.butner@siu.edu>
2020-07-06 11:47:12 -0500

    'Login' -> 'Continue' on LoginComponent, as well as some minor layout changes

--- components/login.tsx
+++ components/login.tsx
@@ -22,27 +22,27 @@ const Login: FunctionComponent<LoginProps> = ({ loginPage, onValueChange }) => {
         <CardGeneric className="fixed bottom-0 w-full md:relative login-buttons" title="Login">
           {!loginPage && <div onClick={onValueChange}><Icon className="absolute top-0 right-0 w-8 h-8 m-auto cursor-pointer text-shadow hover:text-red-500 animated mb-10 card-animated text-bluegrey-300 mr-2 mt-2" path={mdiClose}/></div>}
           <button className="button-google m-auto flex justify-center align-center animated w-full" id="google" onClick={(e): void => handleLoginButtonClick(e.currentTarget.id)}>
-            <div className="flex">
-              <img src="/images/logos/google.png" className="w-10 h-10"/>
-              <p className="ml-4 m-auto">Login with Google</p>
+            <div className="flex justify-between pt-2 pb-2 w-full">
+              <img src="/images/logos/google.png" className="w-8 h-8"/>
+              <div className="m-auto"><p className="ml-4">Continue with Google</p></div>
             </div>
           </button>
           <button className="button-facebook m-auto flex justify-center align-center animated w-full" id="facebook" onClick={(e): void => handleLoginButtonClick(e.currentTarget.id)}>
-            <div className="flex">
-              <img src="/images/logos/facebook.png" className="w-10 h-10"/>
-              <p className="ml-4 m-auto">Login with Facebook</p>
+            <div className="flex justify-between pt-2 pb-2 w-full">
+              <img src="/images/logos/facebook.png" className="w-8 h-8"/>
+              <div className="m-auto"><p className="ml-4">Continue with Facebook</p></div>
             </div>
           </button>
           <button className="button-twitter m-auto flex justify-center align-center animated w-full" id="twitter" onClick={(e): void => handleLoginButtonClick(e.currentTarget.id)}>
-            <div className="flex">
-              <img src="/images/logos/twitter.png" className="w-10 h-10"/>
-              <p className="ml-4 m-auto">Login with Twitter</p>
+            <div className="flex justify-between pt-2 pb-2 w-full">
+              <img src="/images/logos/twitter.png" className="w-8 h-8"/>
+              <div className="m-auto"><p className="ml-4">Continue with Twitter</p></div>
             </div>
           </button>
           <button className="button-spotify m-auto flex justify-center align-center animated w-full" id="spotify" onClick={(e): void => handleLoginButtonClick(e.currentTarget.id)}>
-            <div className="flex">
-              <img src="/images/logos/spotify_white.png" className="w-10 h-10"/>
-              <p className="ml-4 m-auto">Login with Spotify</p>
+            <div className="flex justify-between pt-2 pb-2 w-full">
+              <img src="/images/logos/spotify_white.png" className="w-8 h-8"/>
+              <div className="m-auto"><p className="ml-4">Continue with Spotify</p></div>
             </div>
           </button>
           <div className="text-sm text-center mt-10 text-gray-500">

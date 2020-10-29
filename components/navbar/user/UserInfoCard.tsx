1f1a34298a7e69520a64639645dea46e4c087081
BButner <beau.butner@siu.edu>
2020-10-29 15:47:16 -0500

    Beginnings of UserInfoCard

--- components/navbar/user/UserInfoCard.tsx
+++ components/navbar/user/UserInfoCard.tsx
@@ -4,6 +4,8 @@ import React, { FunctionComponent, useRef, useEffect } from 'react'
 import styles from './userinfocard.module.scss'
 import Closable from 'components/generic/Closable'
 import { clickedElementIsPopupElement } from 'lib/util/PopupUtil'
+import Config from 'config/default.json'
+import Image from 'next/image'
 
 type UserInfoCardProps = {
   self: ModelUserSelf,
@@ -32,9 +34,23 @@ const UserInfoCard: FunctionComponent<UserInfoCardProps> = ({ self, closeFunctio
 
   return (
     <div className={styles['card-wrapper']} ref={card}>
-      <CardGeneric>
+      <CardGeneric className="text-center space-y-6">
         <Closable closeFunction={closeFunction} />
-        <p>{self.username}</p>
+        <div className="m-auto flex justify-center">
+          <Image className="w-full m-auto " width={100} height={100} src={`${Config.bucketUrl}${self.avatarUrl}`} quality={100} priority />
+        </div>
+        <div>
+          <p className="text-lg">Welcome, {self.username}.</p>
+          <p className="text-color-alt">{self.displayName}</p>
+          <p className="text-color-alt text-sm">{self.email}</p>
+        </div>
+        <div className="space-y-2">
+          <div className="flex space-x-2">
+            <button className="w-32">Profile</button>
+            <button className="w-32">Settings</button>
+          </div>
+          <button className="button-red w-full">Logout</button>
+        </div>
       </CardGeneric>
     </div>
   )
---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  images: {
+    domains: ['harmonydev.s3.amazonaws.com']
+  }
+}

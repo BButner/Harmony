96410e32009807fc913fa61894965931a908e339
BButner <beau.butner@siu.edu>
2020-12-05 12:45:39 -0600

    First rendition of song display finished

--- components/generic/card/CardGeneric.tsx
+++ components/generic/card/CardGeneric.tsx
@@ -3,12 +3,13 @@ import React, { FunctionComponent } from 'react'
 type CardGenericProps = {
   className?: string;
   noPadding?: boolean;
+  noFill?: boolean;
   onClick?: any;
 }
 
-const CardGeneric: FunctionComponent<CardGenericProps> = ({ children, className, noPadding, onClick }) => {
+const CardGeneric: FunctionComponent<CardGenericProps> = ({ children, className, noPadding, onClick, noFill }) => {
   return (
-    <div className={`bg-main-200 ${noPadding ? '' : 'p-4'} shadow-xl rounded-std ${className ? className : ''}`} onClick={onClick}>
+    <div className={`${noFill ? '' : 'bg-main-200'} ${noPadding ? '' : 'p-4'} shadow-xl rounded-std ${className ? className : ''}`} onClick={onClick}>
       {children}
     </div>
   )
--- components/service/ServiceSongDisplay.tsx
+++ components/service/ServiceSongDisplay.tsx
@@ -1,3 +1,5 @@
+import Icon from '@mdi/react'
+import { mdiSpotify } from '@mdi/js'
 import CardGeneric from 'components/generic/card/CardGeneric'
 import Closable from 'components/generic/Closable'
 import Pagination from 'components/generic/pagination/Pagination'
@@ -24,19 +26,26 @@ const ServiceSongDisplay: FunctionComponent<ServiceSongDisplayProps> = ({ visibl
   if (!dataSongs) return (<p>loading...</p>)
 
   return (
-    <div className="w-screen flex justify-center items-start">
-      <CardGeneric className="w-96 text-center max-w-96 space-y-2">
+    <div className="w-screen flex justify-center items-start space-x-4">
+      <CardGeneric className="w-96 text-center max-w-96 space-y-2 appear">
         <h1 className="text-xl">{playlist.name}</h1>
-        <p className="text-color-alt text-sm">{playlist.description}</p>
+        <p className="text-color-alt text-sm pb-4">{playlist.description}</p>
+        <Icon className="m-auto text-spotify-500" path={mdiSpotify} size={4} />
+        <div className="pt-2 flex justify-around flex-wrap">
+          <a href={playlist.uri} className="mt-2"><button className="w-28">View</button></a>
+          <button className="button-blue w-28 mt-2">Transfer</button>
+          <button className="button-blue w-28 mt-2">Share</button>
+          <button className="button-red w-28 mt-2">Delete</button>
+        </div>
       </CardGeneric>
       <Closable closeFunction={setVisible} />
-      <Pagination minHeight={(SIZE + 8) * PER_PAGE} schemaFilter={['name', 'artist.name']} allValues={dataSongs} setCurrentValues={setCurrentSongs} perPage={PER_PAGE} searchPrefix='Song' noFlex>
-        {currentSongs.map(song => {
+      <Pagination className="w-2/5" minHeight={(SIZE + 8) * PER_PAGE} schemaFilter={['name', 'artist.name']} allValues={dataSongs} setCurrentValues={setCurrentSongs} perPage={PER_PAGE} searchPrefix='Song' noFlex>
+        {currentSongs.map((song, index) => {
           return (
-            <CSSTransition key={song.id} timeout={275} classNames="fade-slide">
-              <CardGeneric noPadding className={`overflow-hidden flex w-2/5 ${styles['song-card']} m-auto`}>
+            <CSSTransition key={`${song.id}:${index}`} timeout={275} classNames="fade-slide-left">
+              <CardGeneric noPadding className={`overflow-hidden flex w-2/5 ${styles['song-card']} m-auto w-full`} noFill>
                 <img src={song.album.imageHref} width={75} height={75} />
-                <div className="flex justify-between items-center">
+                <div className="flex justify-between items-center bg-main-200 w-full">
                   <div className="pl-2 pr-2">
                     <p>{song.name}</p>
                     <p className="text-sm text-color-alt">

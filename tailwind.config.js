1f4612c71d388247d6f21e4fa64c7591144c65dc
BButner <beau.butner@siu.edu>
2020-12-08 11:50:13 -0600

    Adds TailwindCSS purging

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
     // removeDeprecatedGapUtilities: true,
     // purgeLayersByDefault: true,
   },
-  purge: [],
+  purge: ['./components//*.{js,ts,jsx,tsx}', './pages//*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {

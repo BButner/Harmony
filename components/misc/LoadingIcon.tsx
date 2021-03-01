d7b69d111fa2b8ff9e205a4c4c5762d3fe6a8bb4
BButner <butner.beau@gmail.com>
2021-03-01 16:22:20 -0600

    Adds loading icon

--- components/misc/LoadingIcon.tsx
+++ components/misc/LoadingIcon.tsx
@@ -4,11 +4,8 @@ import styles from './LoadingIcon.module.scss'
 
 export const LoadingIcon: FunctionComponent = () => {
   return (
-    <div className={`flex h-32 space-x-5 mt-52`}>
-      <div className={styles.sphere} />
-      <div className={styles.sphere} />
-      <div className={styles.sphere} />
-      <div className={styles.sphere} />
+    <div className={styles.outer}>
+      <div className={styles.inner}></div>
     </div>
   )
 }
